de6fc4243edb475391f291bd8f7d6b60aa4a8e96
tsukumi <tsukumijima@users.noreply.github.com>
2026-03-11 00:19:44 +0900

    Refactor: レシピに定義された変数に暗黙的に依存しており単独ファイルに切り出す必要性が薄い common/ 以下のシェルスクリプトを削除し、レシピ内で直接 marine/bin/ 以下の CLI ツールを実行するようにする

--- recipe/common/build_vocab.sh
+++
@@ -1,5 +0,0 @@
-# NOTE: the script is supposed to be used in recipes like:
-#     . script.sh
-# Please don't try to run the shell directly.
-
-uv run task build-vocab -- $feature_file_dir $vocab_dir -m $vocab_min_freq
